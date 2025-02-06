0990ff86584174391117f720a818f5662755f99c
jason <cintari@gmail.com>
2025-02-05 16:57:23 -0800

    Sample agent
    ?

--- main.py
+++ main.py
@@ -1,33 +1,56 @@
 import gymnasium as gym
+from agent import BotomyAgent
 from env import CustomEnv
-import logging
-import os
 import random
+from tqdm import tqdm
 
-# Clear terminal (Mac/Linux)
-os.system('clear')
+# hyperparameters
+learning_rate = 0.01
+n_episodes = 100_000
+start_epsilon = 1.0
+epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
+final_epsilon = 0.1
 
-# Configure logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s.%(msecs)03d %(levelname)s: %(message)s',
-    datefmt='%H:%M:%S'
+env = gym.make('CustomEnv-v0')
+env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
+
+agent = BotomyAgent(
+    env=env,
+    learning_rate=learning_rate,
+    initial_epsilon=start_epsilon,
+    epsilon_decay=epsilon_decay,
+    final_epsilon=final_epsilon,
 )
-
-# Create logger
-logger = logging.getLogger(__name__)
-
 if __name__ == "__main__":
-    env = gym.make('CustomEnv-v0')
-    state = env.reset(seed=10, options={
-        "round_length": 10,
-    })
+    for episode in tqdm(range(n_episodes)):
+        obs, info = env.reset(seed=10, options={"round_length": 2})
+        done = False
 
-    episode_over = False
-    while not episode_over:
-        action = random.randint(0, 4)  # Generate a random action from 0 to 4
-        observation, reward, terminated, truncated, info = env.step(action)
+        # play one episode
+        while not done:
+            action = agent.get_action(obs)
+            next_obs, reward, terminated, truncated, info = env.step(action)
 
-        episode_over = terminated or truncated
+            # update the agent
+            agent.update(obs, action, reward, terminated, next_obs)
 
-    env.close()
+            # update if the environment is done and the current obs
+            done = terminated or truncated
+            obs = next_obs
+
+        agent.decay_epsilon()
+
+
+    # env = gym.make('CustomEnv-v0')
+    # state = env.reset(seed=10, options={
+    #     "round_length": 10,
+    # })
+
+    # episode_over = False
+    # while not episode_over:
+    #     action = random.randint(0, 4)  # Generate a random action from 0 to 4
+    #     observation, reward, terminated, truncated, info = env.step(action)
+
+    #     episode_over = terminated or truncated
+
+    # env.close()
