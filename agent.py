91a89e5139394a33c0283b5130534fac7995a113
jason <cintari@gmail.com>
2025-02-05 23:10:58 -0800

    Save/load

--- agent.py
+++ agent.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
 import gymnasium as gym
 import numpy as np
+import pickle
+import os
+from datetime import datetime
 
 
 class BotomyAgent:
@@ -73,3 +76,45 @@ class BotomyAgent:
 
     def decay_epsilon(self):
         self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+
+    def save(self, filepath: str = None) -> str:
+        if filepath is None:
+            # Create models directory if it doesn't exist
+            os.makedirs('models', exist_ok=True)
+            # Generate filename with timestamp
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            filepath = f'models/agent_{timestamp}.pkl'
+            
+        # Save agent state
+        state = {
+            'q_values': self.q_values,
+            'epsilon': self.epsilon,
+            'lr': self.lr,
+            'discount_factor': self.discount_factor,
+            'epsilon_decay': self.epsilon_decay,
+            'final_epsilon': self.final_epsilon,
+            'training_error': self.training_error
+        }
+        
+        with open(filepath, 'wb') as f:
+            pickle.dump(state, f)
+            
+        return filepath
+    
+    @classmethod
+    def load(cls, filepath: str) -> 'BotomyAgent':
+        with open(filepath, 'rb') as f:
+            state = pickle.load(f)
+            
+        agent = cls(
+            learning_rate=state['lr'],
+            epsilon_decay=state['epsilon_decay'],
+            final_epsilon=state['final_epsilon']
+        )
+        
+        agent.q_values = state['q_values']
+        agent.epsilon = state['epsilon']
+        agent.discount_factor = state['discount_factor']
+        agent.training_error = state['training_error']
+        
+        return agent
